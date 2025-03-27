5c8aaeceef0ce18809b9bc0a12d5f697a3bea287
mati012 <mati.quiero.arevalo@gmail.com>
2025-03-27 20:00:26 -0300

    cambios

--- game-events/src/main/java/com/example/game_events/config/SecurityConfig.java
+++ game-events/src/main/java/com/example/game_events/config/SecurityConfig.java
@@ -1,5 +1,73 @@
 package com.example.game_events.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests(authorizeRequests ->
+        authorizeRequests
+            .requestMatchers("/", "/home", "/search", "/events", "/css/**", "/js/**", "/images/**", "/static/**").permitAll()
+            
+            .requestMatchers("/events/*/details").authenticated()
+            .anyRequest().authenticated()
+    )
+    // resto
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/home")
+                    .permitAll()
+            )
+            .logout(logout ->
+                logout
+                    .logoutSuccessUrl("/home")
+                    .permitAll()
+            );
+        
+        return http.build();
+    }
+
     
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password(passwordEncoder().encode("password1"))
+                .roles("USER")
+                .build();
+        
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password(passwordEncoder().encode("password2"))
+                .roles("USER")
+                .build();
+        
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN", "USER")
+                .build();
+        
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
