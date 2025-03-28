987edc32ee1152dc20b68c547130e1a6070c2c07
mati012 <mati.quiero.arevalo@gmail.com>
2025-03-27 22:32:10 -0300

    cambios

--- game-events/src/main/java/com/example/game_events/config/SecurityConfig.java
+++ game-events/src/main/java/com/example/game_events/config/SecurityConfig.java
@@ -5,18 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.game_events.Service.CustomUserDetailsService;
@@ -31,34 +24,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())  // Solo para desarrollo
-        .authorizeHttpRequests(authorizeRequests ->
-        authorizeRequests
-            .requestMatchers("/", "/home", "/search", "/events", "/events/**", "/css/**", "/js/**", "/images/**", 
-                             "/static/**", "/register", "/login").permitAll()
-            .anyRequest().authenticated()
-        )
-        .formLogin(formLogin ->
-            formLogin
-                .loginPage("/login")
-                .defaultSuccessUrl("/home")
-                .permitAll()
-        )
-        .logout(logout ->
-            logout
-                .logoutSuccessUrl("/home")
-                .permitAll()
-        );
-        
+            .csrf(csrf -> csrf.disable())  // Solo para desarrollo
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/", "/home", "/search", "/events", "/events/**", "/css/**", "/js/**", "/images/**", "/static/**", "/register", "/login").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/home")
+                    .permitAll()
+            )
+            .logout(logout ->
+                logout
+                    .logoutSuccessUrl("/home")
+                    .permitAll()
+            );
+
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-    
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -66,7 +57,7 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
